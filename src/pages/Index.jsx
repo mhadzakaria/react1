23abfede843eddce1b63e4219772f81daa211ab0
mhadzakaria <azak808@gmail.com>
2025-10-19 05:50:53 +0700

    basic useState, optimize code

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react"
 import posts from "./../posts.json"
 import Article from "../components/Article"
 
 function Homepage() {
+  const [search, setSearch] = useState("")
+  const changeSearch = (event) => {
+    setSearch(event.target.value)
+  }
   return (
     <>
       <h1>Simple Blog</h1>
       {/* jika kirim props/params selain string pake nya {} */}
-      {posts.map( post => (
-        <Article title={post.title} date={post.date} tags={post.tags}/>
+      <div>Cari : <input type="text" onChange={changeSearch} /></div>
+      <span>Ditemukan 0 data dengan teks {search}</span>
+      {posts.map( (post, index) => (
+        <Article title={post.title} date={post.date} tags={post.tags} key={index } />
       ))
       }
+      {/* ini cara kedua kirim params dengn spesifik key object nya lebih simple */}
+      {posts.map(({title, tags, date}, index) => (
+        <Article {...{title, tags, date}} key={index}/>
+      ))}
     </>
   )
 }
